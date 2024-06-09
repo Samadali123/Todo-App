1529be50a501813866c350d988caf6ea018f9255
Syed Samad ali <114275255+Samadali123@users.noreply.github.com>
2024-06-09 15:22:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,9 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render( <
-    TodoContextProvider >
-    <
-    App / >
-    <
-    ToastContainer / >
-    <
-    /TodoContextProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <TodoContextProvider>
+    <App />
+    <ToastContainer/>
+  </TodoContextProvider>
 );
