dbdb6d0c7a4aa9c79db5f95ae818a876e4a08a3a
Samadali123 <samadali0125@gmail.com>
2024-06-09 14:45:46 +0530

    make app header

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const header = () => {
+    return ( <
+        div className = 'pt-8' >
+
+        <
+        h2 className = 'uppercase text-2xl font-bold text-center' > My < span className = 'text-orange-600' > Todo < /span></h
+        2 >
+
+        <
+        /div>
+    )
+}
+
+export default header
